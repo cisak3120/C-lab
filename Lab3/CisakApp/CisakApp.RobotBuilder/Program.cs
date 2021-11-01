7710b5e8ed8ef5f26dc966796247af698668c99e
Jakub Cisak <cisak3120@gmail.com>
2021-11-01 12:41:43 +0100

    zad 1,2,3

---
+++ Lab3/CisakApp/CisakApp.RobotBuilder/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CisakApp.RobotBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Robot robot = new Robot("BB", 8);
+            robot.DajSygnal();
+            Console.WriteLine(robot.Seria);
+            robot.Uruchom();
+            robot.DajSygnal();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lab3/CisakApp/CisakApp.RobotBuilder/Robot.cs
@@ -0,0 +1,53 @@
+﻿using CisakApp.RobotBuilder.Urządzenia.Wejścia;
+using CisakApp.RobotBuilder.Urządzenia.Wyjścia;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CisakApp.RobotBuilder
+{
+    class Robot
+    {
+        private string kodSerii;
+        private int nrSerii;
+        private bool wlaczony;
+        private Kamera kamera;
+        private Mikrofon mikrofon;
+        private Głośnik głośnik;
+        private Lampa lampa; 
+
+
+
+        public Robot(string kod, int nr)
+        {
+            kodSerii = kod;
+            nrSerii = nr;
+            wlaczony = false;
+            kamera = new Kamera();
+            mikrofon = new Mikrofon();
+            głośnik = new Głośnik();
+            lampa = new Lampa();
+        }
+
+        public string Seria
+        {
+            get { return kodSerii + "-" + nrSerii; }
+            set { }
+        }
+        public void Uruchom()
+        {
+            if( wlaczony == false) {
+                wlaczony = true;
+            }
+        }
+
+        public void DajSygnal()
+        {
+            if(wlaczony == true )
+            Console.WriteLine("Sygnał: Wrrrp bleep");
+            
+        }
+    }
+}
